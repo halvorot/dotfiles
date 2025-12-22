a0bb4a3524e280aba9a05bb0ca60d661df435f97
Halvor Ødegård Teigen <halvorot@gmail.com>
2025-12-22 14:55:24 +0100

    Add Finder preferences for new window target and show all file extensions

--- macos/finder.sh
+++ macos/finder.sh
@@ -4,5 +4,7 @@ source "$(dirname "$0")/lib.sh"
 defaults_write com.apple.finder ShowPathbar bool true
 defaults_write com.apple.finder ShowStatusBar bool false
 defaults_write com.apple.finder AppleShowAllFiles bool true
+defaults_write com.apple.finder NewWindowTarget string "PfHm" # Home folder
+defaults_write NSGlobalDomain AppleShowAllExtensions bool true
 
 restart Finder
