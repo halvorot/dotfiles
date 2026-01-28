a58d9b23e282e7ade289a43618fbcca60964680a
Halvor Ødegård Teigen <halvorot@gmail.com>
2026-01-28 21:02:45 +0100

    Update dock.sh to adjust Dock tile size and magnification settings

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,7 +2,9 @@
 source "$(dirname "$0")/lib.sh"
 
 defaults_write com.apple.dock autohide bool false
-defaults_write com.apple.dock tilesize int 67
+defaults_write com.apple.dock tilesize int 60
+defaults_write com.apple.dock magnification bool true
+defaults_write com.apple.dock largesize int 75
 defaults_write com.apple.dock show-recents bool false
 
 restart Dock
