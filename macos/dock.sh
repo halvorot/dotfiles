4e395aa3adee99145ce40f88ee6fe3866bf4c7f8
Halvor Ødegård Teigen <halvorot@gmail.com>
2026-05-26 20:58:20 +0200

    Adjust Dock tile size in dock.sh from 60 to 52 for improved appearance

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,7 +2,7 @@
 source "$(dirname "$0")/lib.sh"
 
 defaults_write com.apple.dock autohide bool false
-defaults_write com.apple.dock tilesize int 60
+defaults_write com.apple.dock tilesize int 52
 defaults_write com.apple.dock magnification bool true
 defaults_write com.apple.dock largesize int 75
 defaults_write com.apple.dock show-recents bool false
